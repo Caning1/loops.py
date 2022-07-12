735bd4893e9409dc106190ffc5cb30c7f8e1ce7d
Caning1 <107156771+Caning1@users.noreply.github.com>
2022-07-12 00:05:28 -0400

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,34 @@
+for i in range(151):
+    print(i)
+
+for i in range(5,1001,5):
+    print(i)
+
+for i in range(0,101):
+    if i % 5==0:
+        print("Cooding")
+    elif i % 10==0:
+        print("Coding Dojo")
+    else:
+        print(i)
+
+sum = 0 
+for i in range(1,500001,3):
+    sum += i
+    print(sum)
+
+for i in range(2018,-4):
+    print(i)
+
+
+low=2
+high=10
+mult=3
+
+for i in range(low,high):
+    # print(i)
+    if i % mult == 0:
+        print(i)
+
+
+
